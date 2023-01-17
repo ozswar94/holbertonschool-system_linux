51404784496dafca5dd78912a8bf414cbb83d533
Ozswar94 <jordan6heat@hotmail.fr>
2023-01-17 19:04:44 +0100

    first try

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-
 import sys
-import os
 
 
 if __name__ == "__main__":
@@ -15,14 +13,28 @@ if __name__ == "__main__":
 	filename_mem = "/proc/{}/mem".format(sys.argv[1])
 	search = sys.argv[2]
 	replace = sys.argv[3]
+	try:
+		with open(filename_maps, "r") as maps, open(filename_mem, 'rb+', 0) as mem:
+			print("[*] The file {} is Open!".format(filename_maps))
+			print("[*] The file {} is Open!".format(filename_mem))
+	
+			for line in maps.readlines():
+				if "[heap]" in line:
+					heap = line
+					break
 
-	with open(filename_maps, "r") as maps, open(filename_mem, 'r', 0) as mem:
-		for line in maps.readlines():
-			if "[heap]" in line:
-				heap = line
-				break
-		heap = heap.split(" ")[0].split('-')
-		start = int(heap[0], 16)
-		end = int(heap[1], 16)
-		print(heap, hex(start), hex(end))
+			heap = heap.split(" ")[0].split('-')
+			start = int(heap[0], 16)
+			end = int(heap[1], 16)
+			print("[*] Heap start =", hex(start), "end = ", hex(end))
 
+			mem.seek(start)
+			content = mem.read(end - start)
+			offset = content.find(bytes(search, 'ASCII'))
+			print("[*] {} Find in HEAP!".format(search))
+			mem.seek(start + offset)
+			mem.write(bytes(replace + "\0", 'ASCII'))
+			print("[*] {} > {}".format(search, replace))
+	except Exception:
+		print("Error in script !")
+		sys.exit(1)
