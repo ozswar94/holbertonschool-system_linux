f67b69002434ea53b15c3bae07ca1bc42d296912
ozswar94 <jordan6heat@hotmail.fr>
2023-02-07 00:28:41 +0100

    task 7 is done

---
+++ 0x05-libasm/7-puts.asm
@@ -0,0 +1,20 @@
+BITS 64
+
+global asm_puts
+extern asm_strlen
+section .text
+
+asm_puts:
+    push rbp
+    mov rbp, rsp
+    push rdi
+	call asm_strlen
+    mov rdx, rax
+	mov rax, 1
+    mov rdi, 1
+	mov rsi, [rsp]
+	syscall
+    mov rax, rdx
+	mov rsp, rbp
+    pop rbp
+    ret
